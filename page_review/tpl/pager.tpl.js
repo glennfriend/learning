1bef10f9fa5ae300f8b3df746450f7819afb4b2a
turtlemt <turtlemt@gmail.com>
2014-10-02 14:20:28 +0800

    Update template struct

---
+++ page_review/tpl/pager.tpl.js
@@ -0,0 +1,142 @@
+/*
+    pager view template
+
+    變數
+        keyword         - 幫助產生不同變數的值
+        templateView    - mustache.js 在產生樣版所使用的物件
+        templateEvent   - 讓 developer 註冊的 events
+        templateModel   - 給予外界使用的接口
+
+    相依:
+        none
+*/
+(function(keyword){
+
+    var templateView = {
+        init: function( obj )
+        {
+            this.itemPerPage = obj.limit || 3;
+            this.page        = obj.page || 1;
+            this.count       = obj.count;
+        },
+        isFirstPage: function()
+        {
+            return (this.page == 1);
+        },
+        isLastPage: function()
+        {
+            var lastPage = this.getAllPage();
+            return (this.page == lastPage);
+        },
+        getAllPage: function()
+        {
+            return this.count / this.itemPerPage;
+        },
+        renderPrev: function()
+        {
+            return function(text, render) {
+                if ( this.isFirstPage() ) {
+                    return '<li class="disabled"><a>&laquo;</a></li>';
+                }
+                return '<li><a class="' + keyword + '_pagerViewPage" data-page="prev">&laquo;</a></li>';
+            }
+        },
+        renderNext: function()
+        {
+            return function(text, render) {
+                if ( this.isLastPage() ) {
+                    return '<li class="disabled"><a>&raquo;</a></li>';
+                }
+                return '<li><a href="javascript:;" class="' + keyword + '_pagerViewPage" data-page="next">&raquo;</a></li>';
+            }
+        },
+        renderPager: function()
+        {
+            return function(text, render) {
+                var content = '';
+                var allPage = this.getAllPage();
+                for ( var i=0; i<allPage; i++ ) {
+                    var page = i+1;
+                    if ( page == this.page ) {
+                        content += '<li class="active"><a href="javascript:;">'+ page +'</a></li>';
+                    }
+                    else {
+                        content += '<li><a href="javascript:;" class="' + keyword + '_pagerViewPage" data-page="'+ page +'">'+ page +'</a></li>';
+                    }
+                }
+                return content;
+            }
+        }
+    };
+
+    var templateEvent = {
+        events: {
+            pageClick: []
+        },
+        listen: function( eventName, callback )
+        {
+            if ( eventName = 'pageClick' ) {
+                var len = this.events.pageClick.length;
+                this.events.pageClick[len] = callback;
+            }
+            // etc event ....
+        },
+        pageClick: function( data )
+        {
+            for ( fun in this.events.pageClick )
+            {
+                this.events.pageClick[fun]( data );
+            }
+        }
+    };
+
+    var templateModel = {
+        renderName: '',
+        importObject: function(pager)
+        {
+            templateView.init(pager);
+        },
+        listen: function( eventName, callback )
+        {
+            templateEvent.listen( eventName, callback );
+        },
+        setRenderName: function( renderName )
+        {
+            this.renderName = renderName;
+        },
+        render: function()
+        {
+            var templateId = '#' + keyword + 'Template';
+            var template = jQuery(templateId).html();
+            var html = Mustache.render( template, templateView );
+            $(this.renderName).html( html );
+
+            // click page event
+            var myself = this;
+            var className = "." + keyword + "_pagerViewPage";
+            $(className).on("click", function(){
+                var page = $(this).attr("data-page");
+                if ( page == 'next' ) {
+                    templateView.page++;
+                }
+                else if ( page == 'prev' ) {
+                    templateView.page--;
+                }
+                else {
+                    templateView.page = page;
+                }
+
+                templateEvent.pageClick({
+                    page:        templateView.page,
+                    isFirstPage: templateView.isFirstPage(),
+                    isLastPage:  templateView.isLastPage()
+                });
+                myself.render();
+            });
+
+        }
+    };
+
+    this[keyword] = templateModel;
+
+})(theTemplateTempKey);
---
+++ page_review/tpl/review.tpl.js
@@ -0,0 +1,55 @@
+/*
+    review view template
+
+    變數
+        keyword             - 幫助產生不同變數的值
+        templateView        - mustache.js 在產生樣版所使用的物件
+        templateCollections - 該 template 是列出多筆資料
+                              為了不讓資料放置在 model 中 (避免資料污染 & 資料應該屬於 view )
+                              所以由 collections 做 暫存 的地方
+        templateModel       - 給予外界使用的接口
+
+    相依:
+        none
+*/
+(function(keyword){
+
+    var templateView = {
+        init: function( obj )
+        {
+            this.id      = obj.id;
+            this.name    = obj.name;
+            this.rating  = obj.rating;
+            this.date    = obj.date;
+            this.content = obj.content;
+        }
+    };
+
+    var templateCollections = [];
+
+    var templateModel = {
+        renderName: '',
+        items: [],
+        importObjects: function( objects ) {
+            templateCollections = objects;
+        },
+        setRenderName: function( renderName )
+        {
+            this.renderName = renderName;
+        },
+        render: function()
+        {
+            var templateId = '#' + keyword + 'Template';
+            var template = jQuery(templateId).html();
+            var html = '';
+            for ( var index in templateCollections ) {
+                templateView.init( templateCollections[index] );
+                html += Mustache.render( template, templateView );
+            }
+            $(this.renderName).html( html );
+        }
+    };
+
+    this[keyword] = templateModel;
+
+})(theTemplateTempKey);
