4b5d7ad881a1ef24712d4be985f57774dc6d8f10
turtlemt <turtlemt@gmail.com>
2014-10-20 19:06:00 +0800

    add livephp

---
+++ livephp/readme.txt
@@ -0,0 +1,4 @@
+
+修改 .htaccess
+
+    以使用這個 live.js tool
---
+++ livephp/tool/livejs.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '<script type="text/javascript">'
+   . file_get_contents(__DIR__ .'/live.js')
+   . '</script>';
+
+$length = ob_get_length();
+$last_modified = date ("F d Y H:i:s", getlastmod());
+header("Content-Length: $length");
+header("Last-Modified: $last_modified GMT time");
+
+?>
