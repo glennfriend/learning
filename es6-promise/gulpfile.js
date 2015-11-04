87d9dd2c8f416207f29196e8ff195036dac30901
root <root@training3.training3>
2015-11-04 02:00:27 -0800

    Update es6-promise about fetch sample

--- es6-promise/gulpfile.js
+++ es6-promise/gulpfile.js
@@ -4,9 +4,10 @@ var gulp = require("gulp");
  *  front-end resource
  */
 gulp.task('toAssets', function () {
-    gulp.src('./node_modules/jquery/dist/*')                                    .pipe(gulp.dest("./public/assets/jquery/"));
-    gulp.src('./node_modules/bootstrap/dist/**')                                .pipe(gulp.dest("./public/assets/bootstrap/"));
-    gulp.src('./node_modules/es6-promise/dist/**')                              .pipe(gulp.dest("./public/assets/es6-promise/"));
+    gulp.src('./node_modules/jquery/dist/*')                .pipe(gulp.dest("./public/assets/jquery/"));
+    gulp.src('./node_modules/bootstrap/dist/**')            .pipe(gulp.dest("./public/assets/bootstrap/"));
+    gulp.src('./node_modules/es6-promise/dist/**')          .pipe(gulp.dest("./public/assets/es6-promise/"));
+    gulp.src('./node_modules/fetch-polyfill/fetch.js')      .pipe(gulp.dest("./public/assets/fetch-polyfill/"));
 });
 
 // --------------------------------------------------------------------------------
